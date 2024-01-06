aceb673b9d0077cbc1c1cafdce64031864e92dc0
mariuslang <marius@mariuslang.de>
2024-01-06 20:53:53 +0100

    api request implemented

--- pushbell.js
+++ pushbell.js
@@ -1,11 +1,35 @@
+// eslint-disable-next-line func-names
 module.exports = function (RED) {
   function pushbell(config) {
     RED.nodes.createNode(this, config);
     const node = this;
 
+    async function createNotification(body) {
+      node.log(body);
+
+      const result = await fetch('https://us-central1-push-notifications-9cf36.cloudfunctions.net/api/createNotification', {
+        method: 'POST',
+        body: JSON.stringify({
+          title: 'Test Title',
+          description: 'Test Description',
+        }),
+        headers: {
+          Authorization: 'Bearer xyz',
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (result.ok) {
+        node.log('Successful');
+      } else {
+        node.error(result.statusText);
+        node.error(result.status);
+      }
+    }
+
     node.on('input', (msg) => {
       const { payload } = msg;
-      node.send({ payload: msg.payload });
+      createNotification(payload);
     });
   }
 
