579a53f459791798871af8db29f5522787493ad1
mariuslang <lang.marius@t-online.de>
2024-01-07 18:52:34 +0100

    pushbell-config node created

---
+++ pushbell-config.js
@@ -0,0 +1,9 @@
+module.exports = function (RED) {
+  function pushbellConfig(n) {
+    RED.nodes.createNode(this, n);
+    this.name = n.name;
+    this.apiKey = n.apiKey;
+  }
+
+  RED.nodes.registerType('pushbell-config', pushbellConfig);
+};
